5c5a4ee2740696a78baca4e6269cffd0bd5e9154
cmlr4136 <cmlr4136@leeds.ac.uk>
2024-10-31 20:46:33 +0000

    gfg

--- task2.py
+++ task2.py
@@ -1,1 +1,14 @@
+def currency_converter(amount, from_currency, to_currency):
+    
+    conversion_rate = {
+    'GBP': {'CNY': 9.24, 'PHP': 75.00, 'INR': 109.06},
+    'CNY': {'GBP': 0.11, 'PHP': 8.12, 'INR': 11.81},
+    'PHP': {'CNY': 0.12, 'GBP': 0.01, 'INR': 1.45},
+    'INR': {'PHP': 0.69, 'CNY': 0.09, 'GBP': 0.01}
+    }
+
+    if amount < 0:
+        return (0.0)
+    
+    return (amount * conversion_rate[from_currency][to_currency])
 
